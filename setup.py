4153a74415b03fdab5cf248300596f863fad14f5
smazoomder@gmail.com <smazoomder@gmail.com>
2019-11-30 01:58:46 +0600

    documentor 1.1

--- setup.py
+++ setup.py
@@ -11,13 +11,14 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
   name = 'documentor',
   packages = ['documentor'],
-  version = '0.0.0.0.1',
+  package_dir={'docu':'documentor'},
+  version = '0.0.0.1.1',
   long_description=long_description,
   long_description_content_type='text/markdown',
   author = 'Shaon Majumder',
   author_email = 'smazoomder@gmail.com',
   url = 'https://github.com/ShaonMajumder/documentor',
-  download_url = 'https://github.com/ShaonMajumder/documentor/archive/0.0.0.0.1.tar.gz',
-  keywords = ['shaon', 'document generator', 'documentation'], 
+  download_url = 'https://github.com/ShaonMajumder/documentor/archive/0.0.0.1.1.tar.gz',
+  keywords = ['shaon', 'document generator', 'documentation'],
   classifiers = [],
 )
